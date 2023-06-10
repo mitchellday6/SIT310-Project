91b57b4520810a292bcb0b8814872bdd43477834
mitchellday6 <mitchell.day.6@gmail.com>
2023-06-10 23:06:20 +1000

    Add files via upload

---
+++ motor_control_ultrasonic_project/motor_control_ultrasonic_project.ino
@@ -0,0 +1,256 @@
+// defines pins numbers
+const int echoPinLeft = 5;
+const int trigPinLeft = 6;
+
+const int echoPinFront = 11;
+const int trigPinFront = 12;
+
+const int echoPinRight = 2;
+const int trigPinRight = 4;
+
+//distance at which robot should stop from object in cms
+const int safeDistance = 15;
+
+// defines variables
+long duration;
+int distance;
+bool isObject = false;
+int speed = 1;   // speed of vehicle cm/sec
+int angVel = 1000;  //turning speed of vehicle deg/s
+unsigned long endTime = 0;  //this time using millis() that the current movement will stop
+
+int motorright = 9;
+int motorrightdir = 7;
+int motorleft = 10;
+int motorleftdir = 8;
+
+// For serial receive.
+const byte numChars = 11;
+char receivedChars[numChars]; // an array to store the received data
+String received;              // The data as a string
+boolean newData = false;
+
+
+//list of possible states the vehicle could be in
+enum STATES {FORWARD, BACKWARD, LEFT, RIGHT, STOP};
+//current state of the vehicle
+STATES curState = STOP;
+
+void setup()
+{
+  pinMode(motorright, OUTPUT);
+  pinMode(motorleft, OUTPUT);
+  pinMode(motorrightdir, OUTPUT);
+  pinMode(motorleftdir, OUTPUT);
+
+  pinMode(trigPinLeft, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(echoPinLeft, INPUT);   // Sets the echoPin as an Input
+  pinMode(trigPinFront, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPinFront, INPUT);  // Sets the echoPin as an Input
+  pinMode(trigPinRight, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPinRight, INPUT);  // Sets the echoPin as an Input
+  Serial.begin(9600);            // normal printing
+}
+
+void loop()
+{
+  int left = ultrasonic(echoPinLeft, trigPinLeft);
+  int front = ultrasonic(echoPinFront, trigPinFront);
+  int right = ultrasonic(echoPinRight, trigPinRight);
+  
+  if(front < safeDistance || left < safeDistance || right < safeDistance){
+    if(!isObject){
+       isObject = true;
+       stop();
+      }
+  } else {
+    if(isObject){
+      isObject = false;
+    }
+
+    //check the current vehicle state and resume its movement
+    switch(curState){
+      case FORWARD:
+        forward();
+        break;
+      case BACKWARD:
+        backward();
+        break;
+      case LEFT:
+        turnLeft();
+        break;
+      case RIGHT:
+        turnRight();
+        break;
+      default: 
+        stop();
+        break;
+    }
+  }
+  
+  delay(100); // wait for 1/10th of a second.
+
+  recvWithEndMarker();
+  processCommand();
+
+  checkTime();
+}
+
+
+
+int ultrasonic(int echoPin, int trigPin)
+{
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  return distance;
+}
+
+void checkTime(){
+//  if(curState == STOP) return;
+  if(millis() >= endTime){
+    curState = STOP;
+    stop();
+  }
+}
+
+void processCommand()
+{
+  if (newData == true)
+  {
+    String instruction = received.substring(0, 5);
+    String data = received.substring(6, 10);
+    Serial.print(instruction + " received\n");
+    delay(100); // wait for 1/10th of a second.
+    if (instruction == "MOVEF"){
+      setForward(data.toInt());
+    }
+    if (instruction == "MOVEB"){
+      setBackward(data.toInt());
+    }
+    if (instruction == "TURNL"){
+      setLeft(data.toInt());
+    }
+    if (instruction == "TURNR"){
+      setRight(data.toInt());
+    }
+    if(instruction == "STOPA"){
+      curState == STOP;
+      stop();
+    }
+    newData = false;
+  }
+}
+
+void recvWithEndMarker()
+{
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false)
+  {
+    rc = Serial.read();
+
+    if (rc != endMarker)
+    {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars)
+      {
+        ndx = numChars - 1;
+      }
+    }
+    else
+    {
+      receivedChars[ndx] = '\0'; // terminate the string
+      received = String(receivedChars);
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+//these methods set the vehicle movement, start the timer based on calculations and 
+//set the current state of the vehicle
+
+void setForward(int dist){
+  unsigned long time = distance/speed;
+  endTime = millis()+(time*1000);
+  curState = FORWARD;
+  forward();
+}
+
+void setBackward(int dist){
+  unsigned long time = distance/speed;
+  endTime = millis()+(time*1000);
+  curState = BACKWARD;
+  backward();
+}
+
+void setLeft(int deg){
+  unsigned long time = deg/angVel;
+  endTime = millis()+(time*1000);
+  curState = LEFT;
+  turnLeft();
+}
+
+void setRight(int deg){
+  unsigned long time = deg/angVel;
+  endTime = millis()+(time*1000);
+  curState = RIGHT;
+  turnRight();
+}
+
+
+// direction is controlled by the digital pin 7 and 8.
+//  HIGH is backward, LOW is forward
+//  Pins 9 and 10 control speed.
+//  Length of time controls the distance
+
+void forward()
+{
+  digitalWrite(motorrightdir, LOW);
+  analogWrite(motorright, 180);
+  digitalWrite(motorleftdir, LOW);
+  analogWrite(motorleft, 180);
+}
+
+void backward()
+{
+  digitalWrite(motorrightdir, HIGH);
+  analogWrite(motorright, 180);
+  digitalWrite(motorleftdir, HIGH);
+  analogWrite(motorleft, 180);
+}
+void turnLeft()
+{
+  digitalWrite(motorrightdir, LOW);
+  analogWrite(motorright, 180);
+  digitalWrite(motorleftdir, HIGH);
+  analogWrite(motorleft, 180);
+}
+
+void turnRight()
+{
+  digitalWrite(motorrightdir, HIGH);
+  analogWrite(motorright, 180);
+  digitalWrite(motorleftdir, LOW);
+  analogWrite(motorleft, 180);
+}
+
+void stop()
+{
+  analogWrite(motorright, 0);
+  analogWrite(motorleft, 0);
+}
---
+++ services.py
@@ -0,0 +1,125 @@
+import socket
+import threading
+import cv2
+import time
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import serial
+
+
+from robot_interfaces.srv import Move, Turn
+from std_srvs.srv import Empty
+
+class ReadLine:
+    def __init__(self, s):
+        self.buf = bytearray()
+        self.s = s
+
+    def readline(self):
+        i = self.buf.find(b"\n")
+        if i >= 0:
+            r = self.buf[:i+1]
+            self.buf = self.buf[i+1:]
+            return r
+        while True:
+            i = max(1, min(2048, self.s.in_waiting))
+            data = self.s.read(i)
+            i = data.find(b"\n")
+            if i >= 0:
+                r = self.buf + data[:i+1]
+                self.buf[0:] = data[i+1:]
+                return r
+            else:
+                self.buf.extend(data)
+
+
+class RobotServer(Node):
+    robot_response = "no_response"
+    
+    def __init__(self):
+        super().__init__('drone_server')
+
+        self.robot_response = "no_response"
+        self.srv = self.create_service(Move, 'move_forward', self.move_forward_callback)
+        self.srv = self.create_service(Move, 'move_backward', self.move_backward_callback)
+        self.srv = self.create_service(Turn, 'turn_left', self.turn_left_callback)
+        self.srv = self.create_service(Turn, 'turn_right', self.turn_right_callback)
+        self.srv = self.create_service(Move, 'stop', self.stop_callback)
+        
+    def send_command(self, msg):
+        print("Sending Command: "+ msg)
+        ser = serial.Serial('/dev/ttyACM0')
+        msg += "\n"
+        ser.write(msg.encode())
+        # reply = ser.readline()
+        # self.get_logger().info(reply)
+        ser.close()
+        reply = "Msg Sent"
+        return reply
+
+
+    def move_forward_callback(self, request, response):
+        self.get_logger().info('Incoming request: Move Forward: %dcm' % (request.distance))
+        command = "MOVEF:%d" % request.distance
+        # command = "MOVEF:1000"
+        reply = self.send_command(command)
+        response.result = "Sent"
+        return response
+
+
+    def move_backward_callback(self, request, response):
+        self.get_logger().info('Incoming request: Move Backward: %dcm' % (request.distance))
+        command = "MOVEB:%d" % request.distance
+        # command = "MOVEB:1000"
+        reply = self.send_command(command)
+        response.result = "Sent"
+        return response
+
+    def turn_right_callback(self, request, response):
+        self.get_logger().info('Incoming request: Turn Right: %d degrees' % (request.deg))
+        command = "TURNR:%d" % request.deg
+        command = "TURNR:1000"
+        reply = self.send_command(command)
+        response.result = "Sent"
+        return response
+    
+    def turn_left_callback(self, request, response):
+        self.get_logger().info('Incoming request: Turn Left: %d degrees' % (request.deg))
+        command = "TURNL:%d" % request.deg
+        # command = "TURNL:1000"
+        reply = self.send_command(command)
+        response.result = "Sent"
+        return response
+
+    def stop_callback(self, request, response):
+        self.get_logger().info('Incoming request: Stop')
+        command = "STOPA:0000"
+        reply = self.send_command(command)
+        response.result = "Sent"
+        return response
+
+
+    
+                
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = RobotServer()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    # Destroy the node explicitly
+    # (optional - Done automatically when node is garbage collected)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
